cdbb2354174df85815161416fa45ace914fb6d93
Aamani6 <lkolla@yahoo.com>
2017-10-18 15:17:07 -0500

    New code for Sam project

---
+++ src/test/java/Explore/Tata.java
@@ -0,0 +1,25 @@
+package Explore;
+
+public class Tata {
+	
+	String empname;
+	int empno;
+	String empaddress;
+
+	public static void main(String[] args) {
+		
+		Tata T = new Tata();
+		T.empname = "Sam";
+		T.empno = 1;
+		T.empaddress = "Palatine";
+		
+		System.out.println(T.empname);
+		System.out.println(T.empno);
+		System.out.println(T.empaddress);
+		
+		
+		
+
+	}
+
+}
